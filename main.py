0284ef0b3d9f766f0d6219fa755cadc4baf134ef
jmerc77 <24909068+jmerc77@users.noreply.github.com>
2018-12-19 01:58:50 -0500

    major updates and minor fixes
    - added comments to help show flow
    - fixed a few bugs
    - cleaned up redundant logic in a few areas
    - moved nubs to opt.ini

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ USE_SCAD_THREAD_TRAVERSAL = False
 STL_DIR = "_stls"#name gets tacked on later...
 PREV_DIR = "maze_previews"
 
+#tries to get the path to openscad
 def openscad():
     try:
         if OPENSCAD_PATH:
@@ -45,7 +46,7 @@ def openscad():
     # Default to linux-friendly CLI program name
     return "openscad"
 
-
+#prepares folders
 def prepwd():
     # Linux and other systems that use PATH variables don't need an absolute path configured.
     # if os.path.exists(openscad_exe) == False:
@@ -60,6 +61,7 @@ def prepwd():
         rmtree(PREV_DIR)
     os.mkdir(PREV_DIR)  # Default perms: world-writable
 
+#checks threading availability
 def has_scad_threading():
     cmd = [openscad(), "--help"]
     out = str(sp.check_output(cmd)).lower()
@@ -67,14 +69,14 @@ def has_scad_threading():
         return True
     return False
 
-
+#checks version
 def scad_version():
     cmd = [openscad(), "--version"]
-    ver=sp.run(cmd,capture_output=True).stderr
+    ver=sp.run(cmd,capture_output=True).stdout
     ver=str(ver).replace("\\r","").replace("b'OpenSCAD version ","").replace("\\n'","").replace("-",".").split(".")	
     return (int(ver[0]), int(ver[1]), int(ver[2])) if ver else ()
 
-
+#runs the scad
 def execscad(threadid=0):
     print("Executing OpenSCAD script...")
     cmd = [openscad()]
@@ -88,57 +90,83 @@ def execscad(threadid=0):
         ]
     )
     sp.run(cmd)
-    
+
+#updates the possible ways to cut the maze
 def udnbers(n, vi, nc, mw, mh, stag):
+    #with every tile
     for y in range(0, mh):
         for x in range(0, mw):
+            #shift the vertical edge
             x3 = int((x + stag[y]) % mw)
+            #next tile coords
             x2 = [x - 1, x + 1, x, x]
             y2 = [y, y, y - 1, y + 1]
+            #look arround
             for i in range(0, 4):
+                #did we shift the edge?
                 if stag[y] % mw > 0:
+                    #shift the next tile coords too
                     x2[i] = int((x2[i] + mw) % mw)
                 else:
+                    #constrain to bounds otherwise
                     if x2[i] < 0:
                         x2[i] = 0
                     if x2[i] > mw - 1:
                         x2[i] = mw - 1
+                #is this cuttable and not out of bounds?
                 if (
                     not ((x3 == 0 and i == 0) or (x3 == mh - 1 and i == 1))
                     and y2[i] > -1
                     and y2[i] < mh
                 ):
+                    #mark cuttable if we have not been there
                     n[x, y, i] = vi[int(x2[i]), int(y2[i])] == 0
                 else:
+                    #mark not cuttable
                     n[x, y, i] = 0
+            #update count of cuttable tiles
             nc[x, y] = len(np.argwhere(n[x, y].astype("int")))
 
-
+#makes a maze
 def genmaze(mw, mh, stag):
+    #where we have cut a path already
     visited = np.zeros(mw * mh)
+    #number of possible ways to cut
     nbercount = np.zeros(mw * mh)
+    #possible ways to cut
     nbers = np.ones(mw * mh * 4)
+    #walls of the maze tiles
+    # walls are: 0=L 1=R 2=U 3=D
     walls = np.ones(mw * mh * 4)
+    #start here
     r = rd.randint(0, mw*mh-1)
+    #mark start as visited
+    #number of places we have cut a path already
     vcount = 1
     visited[r] = 1
+    #to make things easier
     visited = visited.reshape([mw, mh])
     nbers = nbers.reshape([mw, mh, 4])
     nbercount = nbercount.reshape([mw, mh])
     walls = walls.reshape([mw, mh, 4])
+    
+    #update the possible ways to cut
     udnbers(nbers, visited, nbercount, mw, mh, stag)
+    #loop until maze is completed
     while vcount < (mw * mh):
+        #all places we can continue cutting
         v = np.transpose(np.nonzero(np.logical_and(visited == 1, nbercount > 0)))
-        # choose branch
+        # choose a tile to cut from
         r = rd.randint(0, len(v) - 1)
         c = v[r]
-        # choose wall to break
+        # choose wall to cut
         r = rd.randint(0, nbercount[c[0], c[1]] - 1)
         n = np.argwhere(nbers[c[0], c[1]])[r]
-        # break wall
+        # cut the wall
         walls[c[0], c[1], n] = 0
+        #temp for the tile we are cutting
         c2 = c
-        # walls: 0=L 1=R 2=U 3=D
+        #the other side of the wall
         if n == 0:
             n2 = 1
             c2[0] = c[0] - 1
@@ -151,77 +179,117 @@ def genmaze(mw, mh, stag):
         else:
             n2 = 2
             c2[1] = c[1] + 1
+        #wrap horizontally
         c2[0] = int((c2[0] + mw) % mw)
+        #mark as visited
         visited[c2[0], c2[1]] = 1
+        #cut the other side
         walls[c2[0], c2[1], n2] = 0
+        #update the possible ways to cut again
         udnbers(nbers, visited, nbercount, mw, mh, stag)
+        #update the number of places we have cut a path already
         vcount = vcount + 1
-    return walls#lrud
+    return walls
 
+#makes and writes the preview image
 def preview(maze):
-    im = Image.new("L", [2 * mw + 1, 2 * mh + 1], 0)
-    if ((i == 0 and shell < shells - 1) or (i == 1 and shell > 0)) and tpp != 1:
-        im.putpixel((1 + ex * 2, 0), 255)
-        im.putpixel((1 + st * 2, mh * 2), 255)
+        #a new image
+        im = Image.new("L", [2 * mw + 1, 2 * mh + 1], 0)
+        #start and end
+        im.putpixel((1 + ex * 2, 0), 255)#end
+        im.putpixel((1 + st * 2, mh * 2), 255)#start
         for y in range(0, mh):
             for x in range(0, mw):
+                #tile pos
                 imx = 1 + x * 2
                 imy = 1 + y * 2
+                #wall pixel coords
                 imnx = [imx - 1, imx + 1, imx, imx]
                 imny = [imy, imy, imy - 1, imy + 1]
+                #center of tile
                 im.putpixel((imx, imy), 255)
+                #check walls
                 for idx in range(0, 4):
+                    #no wall?
                     if maze[x, y, idx] == 0:
+                        #cut a hole!
                         im.putpixel((imnx[idx], imny[idx]), 255)
+        #transition shell maze 2?
         if tpp == 2:
+            #save as maze 2
             im.save(os.path.join(os.getcwd(), PREV_DIR, str(shell + 1) + "a.png"))
         else:
+            #save as maze 1
             im.save(os.path.join(os.getcwd(), PREV_DIR, str(shell + 1) + ".png"))
-    
+
+#finds the lengths from a start to all ends of a maze  
 def solver(maze,s):
-    branches=[[s,mh-1,0,0,4]]
-    solved=False
+    #start here
+    branches=[[s,mh-1,0,0,4]]#x,y,length,downcnt,last
+    #return value
     ret=[]
+    #loop until return value is full
     while len(ret)<mw:
+        #temporarlily store new branches here
         temp=[]
+        #loop through all current branches
         for branch in branches:
             x=branch[0]
             y=branch[1]
             length=branch[2]
+            #count for how many times we go down toward start
             downcnt=branch[3]
+            #must not back track.
             last=branch[4]
+            #walls at x,y in the maze
             here=maze[x,y]
+            #how many openings at x,y in maze
             opencnt=4-np.sum(here)
+            #is this a posible end?
             if y==0:
+                #include this length in return value.
+                #modified a little for extra difficulty levels.
                 ret.append(length+downcnt/mh)
+            #can we move on?
             if opencnt>0:
+                #move on but do not bactrack.
                 if here[0]==0 and last!=0:
+                    #left
                     temp.append([(x+mw-1)%mw,y,length+1,downcnt,1])
                 if here[1]==0 and last!=1:
+                    #right
                     temp.append([(x+1)%mw,y,length+1,downcnt,0])
                     
                 if here[2]==0 and last!=2:
+                    #up
                     temp.append([x,y-1,length+1,downcnt,3])
                 if here[3]==0 and last!=3:
+                    #down
                     temp.append([x,y+1,length+1,downcnt+1,2])
+        #copy the new branches over the old branches
         branches=temp.copy()
     return ret
 
+#chooses a maze path (start and end) based on difficulty
 def choose_path(maze):
     global st
     global ex
-    #difficulty
     #get path lengths...
     lengths=[]
     for s in range(mw):
-        #find the ways out...
+        #find the lengths from this start to all ends
         lengths.append(solver(maze,s))
+    #get sorted indexes
     sortedlengthidxs=np.argsort(np.asarray(lengths).flatten())
+    #choose one
     chosen=sortedlengthidxs[int(difficulty/101*len(sortedlengthidxs))]
-    st=chosen//mw
-    ex=chosen%mw
+    #assign start and end
+    st=chosen//mw#start
+    ex=chosen%mw#end
+    #make and write preview image
     preview(maze)
-
+    
+#makes parts
 def gen():
     global shell
     global d2
@@ -229,71 +297,105 @@ def gen():
     global mw
     global i
     global tpp
+    #are we done yet?
     if shell < shells:
+        #debug halt
         if shell == halt:
             exit()
+        #is the next shell the transition?
         if shell + 1 > 0 and shell + 1 < shells and shell + 1 == tp and tpp < 1:
             tpp = -1
+        #part status
         if tpp < 1:
             print("part: " + str(shell + 1))
+        #wall thickness
         wt = mwt
+        #are we not in transitioning stage 2?
         if tpp < 1:
+            #is this the first?
             if shell == 0:
+                #set the diameter
                 d = (mw * us * p) / np.pi + wt - marge * 2
             else:
+                #are we transitioning?
                 if shell == tp:
+                    #keep the diameter the same
                     d = d2
                 else:
+                    #set the diameter
                     d = d2 + us + wt + marge * 2
+                #is the maze on the outside?
                 if i == 0:
+                    #set the maze width
                     mw = int(math.ceil((d / p + us) * np.pi / 2 / us))
+                    #is this the shell before the lid?
                     if shell == (shells - 2):
+                        #increase maze height
                         mh += 1
                 else:
+                    #is this the lid?
                     if shell == (shells - 1):
+                        #set the maze width
                         mw = int(math.ceil((d / p + us) * np.pi / 2 / us))
                     else:
+                        #set the maze width
                         mw = int(math.ceil((d2 / p + us) * np.pi / 2 / us))
+                #increase maze height
                 mh += 1
         else:
+            #set the diameter
             d = d2 + us + wt + marge * 2
+            #set the maze width
             mw = int(math.ceil((d / p + us) * np.pi / 2 / us))
+            #increase maze height
             mh += 1
 
-        # stag/shift
+        # shift
         stag = np.zeros(mh)
+        #is it a random shift?
         if stagmode in (1, 2):
+            #loop through y
             for y in range(0, mh):
+                #are we at end or shift mode is random?
                 if y == 0 or stagmode == 1:
+                    #random shift
                     stag[y] = rd.randint(0, mh - 1)
                 else:
+                    #random shift offset
                     stag[y] = stag[y - 1] + rd.randint(0, mh - 1)
+        #is it a twist shift?
         elif stagmode == 3:
+            #twist it!
             stag = np.multiply(np.arange(0, mh), stagconst).astype("int")
-        # maze
-        
-        #st = rd.randint(0, mw - 1)
-        #ex = rd.randint(0, mw - 1)
-        marr = genmaze(int(mw), int(mh), stag)
-        choose_path(marr)
-        matrix = []
-        for y in range(0, mh):
-            row = []
-            for x in range(0, mw * p):
-                x2 = x % mw
-                r = marr[x2, y, 1] == 0
-                u = marr[x2, y, 3] == 0
-                if u and r:
-                    row.append("3")
-                elif u:
-                    row.append("2")
-                elif r:
-                    row.append("1")
-                else:
-                    row.append("0")
-            matrix.append(f"[{','.join(row)}]")
-        s = f"[{','.join(matrix)}];"
-
+            
+        #do we even have a maze with this part?
+        if ((i == 0 and shell < shells - 1) or (i == 1 and shell > 0)) and tpp != 1:
+            # maze
+            marr = genmaze(int(mw), int(mh), stag)
+            #get the path we want
+            choose_path(marr)
+            #convert to string
+            matrix = []
+            for y in range(0, mh):
+                row = []
+                for x in range(0, mw * p):
+                    x2 = x % mw
+                    r = marr[x2, y, 1] == 0
+                    u = marr[x2, y, 3] == 0
+                    if u and r:
+                        row.append("3")
+                    elif u:
+                        row.append("2")
+                    elif r:
+                        row.append("1")
+                    else:
+                        row.append("0")
+                matrix.append(f"[{','.join(row)}]")
+            s = f"[{','.join(matrix)}];"
+        else:
+            #empty maze
+            s="[];"
+        #write the maze
         if tpp < 1:
             maze_num = 1
             open_mode = "w"
@@ -313,15 +415,18 @@ def gen():
                     ]
                 )
             )
-
+        #non lid
         base = 1
         lid = 0
-        if shell == shells - 1:
+        #is it the lid?
+        if shell > shells - 2:
+            #lid
             lid = 1
             base = 0
-        if shell > shells - 2:
+            #no more to go
             mos = 0
         else:
+            #how many are left to go
             mos = shells - shell - 2
         with open("config.scad", "w+") as cfg:
             cfg.write(
@@ -342,10 +447,12 @@ def gen():
                     ]
                 )
             )
+        #save diameter of this one for later
         if shell < shells - 2:
             d2 = d
-
+        #was this the transition shell?
         if shell > 0 and shell < shells and shell == tp and tpp < 1:
+            #get ready for transition stage 2
             if i == 0:  # double nub transition
                 tpp = 1
                 i = 1
@@ -354,14 +461,20 @@ def gen():
                 i = 0
         else:
             tpp = 0
+        #are we done with this shell?
         if tpp < 1:
+            #make it!
             execscad()
+            #on to the next
             shell = shell + 1
+        #not done making parts
         return False
     else:
+        #all done!
         return True
 
-def ReadOpt():
+#reads opt.ini
+def readOpt():
     global shells
     global marge
     global us
@@ -369,6 +482,7 @@ def ReadOpt():
     global mw
     global mwt
     global i
+    global p
     global tp
     global STL_DIR
     config = configparser.ConfigParser()
@@ -377,12 +491,12 @@ def ReadOpt():
         print("ERROR: No DEFAULT section in opt.ini\n")
         exit(1)
     config = config["DEFAULT"]
-
-    shells = config.getint("levels") + 1  # levels
+    p = abs(config.getint("nubs")-2) + 2
+    shells = config.getint("levels") + 1
     marge = config.getfloat("tolerance")
     i = int(config.getboolean("maze_inside"))
     tp = config.getint("transition_shell")
-    if tp >= shells:
+    if tp >= shells-1:
         tp = 0
     us = config.getfloat("spacing")
     mh = config.getint("units_tall")
@@ -391,40 +505,41 @@ def ReadOpt():
     name = config.get("name")
     STL_DIR=name+STL_DIR
     
-
 if __name__ == "__main__":
-    ReadOpt()
+    #read opt.ini
+    readOpt()
     try:
+        #prep folders
         prepwd()
+        
         # get scad version:
-        if has_scad_threading():
-            USE_SCAD_THREAD_TRAVERSAL = (
-                input("multi-threading available. use it(y/n)?").lower() == "y"
-            )
-
         version = scad_version()
         if version[0] < 2015:
             print("ERROR: invalid scad version. must be at least 2015.xx.xx .\n")
             exit(1)
+        #do we have threading?
+        if has_scad_threading():
+            USE_SCAD_THREAD_TRAVERSAL = (
+                input("multi-threading available. use it(y/n)?").lower() == "y"
+            )
     except FileNotFoundError:
         print("ERROR: Could not find OpenSCAD: " + openscad()+"\n")
         exit(1)
-
+    #init vars
+    st=0
+    ex=0
     d2 = 0
     shell = 0
-    # make parts:
-    p = abs(int(input("nub count (0=2 nubs,1=3 nubs,2=4 nubs, ...):"))) + 2
     tpp = 0
-    '''hbias = abs(
-        int(input("complexity (horizontal-down bias); 0=none >0= bias; larger= more difficult:"))*100
-    )'''
+    stagconst = 0
+    #maze options
     difficulty=abs(float(input("difficulty (length of path); 0.0 (easy) to 100.0 (hard): ")))
     if difficulty>100:
         difficulty=100
-    stagconst = 0
     stagmode = int(input("shift mode (0=none 1=random 2=random change 3=twist):"))
     if stagmode == 3:
         stagconst = abs(int(input("twist amount:")))
+    # make parts:
     while not gen():
         continue
     print("done!")
